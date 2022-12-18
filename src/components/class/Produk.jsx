f50ec1c96109ff3ec448a2044d2b3dfe888c3ad1
noffrizal <zaimjr@gmail.com>
2022-12-18 22:12:28 +0700

    update

---
+++ src/components/class/Produk.jsx
@@ -0,0 +1,11 @@
+import React from 'react'
+
+class Produk extends React.Component {
+    render(){
+        return <div className='box-stock'>
+            
+        </div>
+    }
+}
+
+export default Produk
